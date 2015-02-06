b1f306e95014d85fb24c09a29849340941099183
mcarenash <mariacamilaarenasd@gmail.com>
2015-02-06 14:55:29 -0800

    Working App

--- todo/app/controllers/tasks_controller.rb
+++ todo/app/controllers/tasks_controller.rb
@@ -1,7 +1,49 @@
 class TasksController < ApplicationController
+
 	def index
 		@tasks = Task.all
 		render :index
 	end
+
+	def new
+		@task = Task.new
+		render :new
+	end
+
+	def create
+		new_task = params.require(:task).permit(:content, :complete)
+		task     = Task.create(new_task)
+		redirect_to "/tasks/#{task.id}"
+	end
+
+	def show
+		id    = params[:id]
+		@task = Task.find(id)
+		render :show
+	end
+
+	def edit 
+		id = params[:id]
+		@task = Task.find(id)
+		render :edit
+	end
+
+	def update
+		task_id = params[:id]
+        task = Task.find(task_id)
+        updated_attributes = params.require(:task).permit(:content, :complete)
+        task.update_attributes(updated_attributes)
+        redirect_to task_path
+	
+	end
+
+	def destroy
+		id = params[:id]
+        task = Task.find(id)
+        task.destroy
+        redirect_to "/tasks"
+	end
+
+
 
 end
--- todo/config/routes.rb
+++ todo/config/routes.rb
@@ -15,7 +15,15 @@ Rails.application.routes.draw do
   get '/contact', to: 'site#contact'
   get '/about', to: 'site#about'
 
-  get '/tasks', to: "tasks#index"
+  get '/tasks', to: "tasks#index", as: "tasks"
+  get '/tasks/new', to: 'tasks#new', as:"new_task"
 
- 
+  get '/tasks/:id', to: 'tasks#show', as:'task'
+
+  get '/tasks/:id/edit', to: 'tasks#edit', as: "edit_task"     
+
+  post "/tasks", to: "tasks#create"
+  patch "/tasks/:id", to: "tasks#update"
+  delete '/tasks/:id', to: "tasks#destroy"
+
 end
